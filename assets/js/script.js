679f35c42eed5a5fd6f306ba378f2cbbaa0c6694
DamienPoix <damienpoix@live.fr>
2018-07-18 12:07:33 +0200

    commentaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var c = document.getElementById('canvasba');
 console.log(document.getElementById('canvasba'));
 var ctx = c.getContext('2d');
+//rectangle de base pour la maison
 ctx.beginPath();
 ctx.moveTo(100,100);
 ctx.lineTo(250,100);
@@ -9,6 +10,7 @@ ctx.lineTo(100,250);
 ctx.closePath();
 ctx.fillStyle = "lightblue";
 ctx.fill();
+//triangle pour la maison
 ctx.beginPath();
 ctx.moveTo(100,100);
 ctx.lineTo(175,50);
@@ -17,6 +19,7 @@ ctx.lineTo(100,100)
 ctx.closePath();
 ctx.fillStyle = "#d7823e";
 ctx.fill();
+//fenetre 1
 ctx.beginPath();
 ctx.moveTo(110,110);
 ctx.lineTo(150,110);
@@ -25,6 +28,7 @@ ctx.lineTo(110,150);
 ctx.closePath();
 ctx.fillStyle = "silver";
 ctx.fill();
+//fenetre 2
 ctx.beginPath();
 ctx.moveTo(200,110);
 ctx.lineTo(240,110);
@@ -33,6 +37,7 @@ ctx.lineTo(200,150);
 ctx.closePath();
 ctx.fillStyle = "silver";
 ctx.fill();
+//porte de la maison
 ctx.beginPath();
 ctx.moveTo(150,190);
 ctx.lineTo(200,190);
